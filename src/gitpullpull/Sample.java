1de592ab79f47ef0af1f915bb628f52f409d6dd1
srisgganesh <88srisg27@gmail.com>
2022-04-06 11:53:11 +0530

    second commit ganesh

--- src/gitpullpull/Sample.java
+++ src/gitpullpull/Sample.java
@@ -9,5 +9,7 @@ public static void main(String []args)
 	System.out.println("Hello world from Thamarai Kannan");
 	
 	System.out.println("Hello world from Mark");
+	
+	System.out.println("Hello world from ganesh commit 2");
 	}
 }
